92fe5bb05605d3442eae6e30ca5fda9e9698bc42
sowmya <sowmya.t@everest.engineering>
2026-01-02 22:57:47 +0530

    Update the services for add the get all tasks method

--- src/services/TaskServices.ts
+++ src/services/TaskServices.ts
@@ -3,10 +3,15 @@ import TaskType from "../types/TaskType";
 
 const collectionTask = db.collection("tasks")
 
-export const addTheTask = async(taskdata:TaskType)=>{
+export const addTheTask = async (taskdata: TaskType) => {
     const task = await collectionTask.doc(taskdata.id).set(taskdata)
-    console.log("show task data",task);
+    console.log("show task data", task);
     return taskdata;
 }
 
+export const getTheTask = async () => {
+    const allTasks = await collectionTask.get()
+    const tasks = allTasks.docs.map(doc => doc.data())
+    return tasks
+}
 

4eea1f700eabdb27def079eaf9f3daf98620ec25
sowmya <sowmya.t@everest.engineering>
2026-01-03 10:18:41 +0530

    Update the router for add remove task functionality

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,7 @@
 import Router from "express"
-import { addTask, getTask } from "../controller/TaskController"
+import { addTask, getTask, taskDelete } from "../controller/TaskController"
 
 export const router = Router()
 router.post('/addnewTask', addTask)
 router.get('/getTasks',getTask)
+router.delete('/deleteTask/:id',taskDelete)

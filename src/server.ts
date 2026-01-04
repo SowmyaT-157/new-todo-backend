877b34515b10c7bfade72761899f7fe8f6629a86
sowmya <sowmya.t@everest.engineering>
2026-01-04 23:55:58 +0530

    Updathe the server for add .env

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,11 @@ import cors from "cors"
 const app = express();
 app.use(express.json());
 
+const PORT = process.env.PORT
+
 app.use(cors())
 app.use("/", router)
-app.listen(3008, () => {
-    console.log("Server running on http://localhost:3008");
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
 export default app;

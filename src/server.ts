65a0555b2a317e463ca638b403342cc01ecf1226
sowmya <sowmya.t@everest.engineering>
2026-01-04 18:58:26 +0530

    Update the server for add the cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { router } from './routes/router';
+import cors from "cors"
 
 const app = express();
 app.use(express.json());
 
+app.use(cors())
 app.use("/", router)
 app.listen(3008, () => {
     console.log("Server running on http://localhost:3008");

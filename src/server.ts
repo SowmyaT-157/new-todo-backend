ef2d4a9c87e4c28f136007d60dcdafb37eb01fb9
sowmya <sowmya.t@everest.engineering>
2026-01-05 10:23:54 +0530

    Update the server for remove the cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express';
 import { router } from './routes/router';
-import cors from "cors"
-
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT
-app.use(cors())
 
 app.use("/", router)
 app.listen(PORT, () => {

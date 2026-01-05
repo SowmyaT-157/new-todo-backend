fdc9fe45448cccdf76ee23898b472de2315df67f
sowmya <sowmya.t@everest.engineering>
2026-01-05 09:53:27 +0530

    Update the server

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import { router } from './routes/router';
 import cors from "cors"
 
+
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT
-
 app.use(cors())
+
 app.use("/", router)
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
